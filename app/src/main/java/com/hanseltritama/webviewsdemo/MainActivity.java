66273e03f750eba1c5424d37d1f154489bb88238
Hansel Tritama <10084360+hanselgunawan@users.noreply.github.com>
2020-04-25 17:22:26 -0700

    added webviews

--- app/src/main/java/com/hanseltritama/webviewsdemo/MainActivity.java
+++ app/src/main/java/com/hanseltritama/webviewsdemo/MainActivity.java
@@ -3,6 +3,8 @@ package com.hanseltritama.webviewsdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.my_webview);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        // To display URL
+//        webView.loadUrl("https://www.google.com");
+
+        // To display HTML
+        webView.loadData("<html><body><h1>Hello!</h1><p>Hehehehe</p></body></html>", "text/html", "UTF-8");
+
     }
 }
